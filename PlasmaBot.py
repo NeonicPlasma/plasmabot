af223898d51e735130cd4970c0660cc437cd2586
NeonicPlasma <43083380+NeonicPlasma@users.noreply.github.com>
2018-09-26 07:11:23 +1200

    Update PlasmaBot.py

--- PlasmaBot.py
+++ PlasmaBot.py
@@ -36,7 +36,7 @@ bot.remove_command('help')
 
 # Minigame Related Variables:
 
-minigameOpen = True
+minigameRunning = False
 minigameParticipants = []
 eliminationOrder = []
 
@@ -108,20 +108,20 @@ async def toggleminigames(ctx):
         
 @bot.command()
 async def bombminigame(ctx, mode):
-    ourMinigameOpen = minigameOpen
     user = ctx.message.author
     userid = user.id
     channel = ctx.message.channel
     if mode == 'create':
         await ctx.send('testing testing 123')
-        if ourMinigameOpen == True:
+        await ctx.send(str(minigameRunning))
+        if minigameRunning == False:
             await ctx.send('**A game of __Pass The Bomb__ has been started!** \nPeople who would like to play can use the `p!bombminigame join` command to participate in the minigame!')
-            minigameOpen = False
+            minigameRunning = True
             currentHost = user
         else:
             await ctx.send("A minigame is already happening! Please wait until the minigame has finished.")
     elif mode == 'join':
-        if ourMinigameOpen == False:
+        if minigameRunning == True:
             if userid in minigameParticipants:
                 await ctx.send("**You have quit the minigame!** The contestant count is now **" + str(len(minigameParticipants)) + "**. If you would like to rejoin, use the `p!bombminigame join` command to participate again.")
                 minigameParticipants.remove(user)
